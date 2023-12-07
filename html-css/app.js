a14dbc775e696e839a2df0f86f73ce85a956f03c
NEGIBOSE <ryo0827ngsh@icloud.com>
2023-12-07 10:53:45 +0900

    ボタンが押されたら、入力された値をlocalStorageに保存する

--- html-css/app.js
+++ html-css/app.js
@@ -12,4 +12,12 @@
       document.getElementById("js-password").value = storedPassword;
     }
   });
+  function signIn() {
+    // ボタンが押されたら、入力された値をlocalStorageに保存する
+    const email = document.getElementById("js-email").value;
+    const password = document.getElementById("js-password").value;
+
+    localStorage.setItem("storedEmail", email);
+    localStorage.setItem("storedPassword", password);
+  }
 })();
