0b04d647a4af45a8f7c9ebdb4fe3ba8d84d57c33
NEGIBOSE <ryo0827ngsh@icloud.com>
2023-12-07 12:45:21 +0900

    パスワードの可視化スイッチの追加

--- html-css/app.js
+++ html-css/app.js
@@ -1,23 +1,26 @@
 (() => {
-  document.addEventListener("DOMContentLoaded", function () {
-    // ページが読み込まれたときに、localStorageからデータを取得してフォームにセットする
-    const storedEmail = localStorage.getItem("storedEmail");
-    const storedPassword = localStorage.getItem("storedPassword");
-
-    if (storedEmail) {
-      document.getElementById("js-email").value = storedEmail;
-    }
-
-    if (storedPassword) {
-      document.getElementById("js-password").value = storedPassword;
-    }
-  });
-  function signIn() {
-    // ボタンが押されたら、入力された値をlocalStorageに保存する
+  // ログイン関数
+  window.signIn = function () {
     const email = document.getElementById("js-email").value;
     const password = document.getElementById("js-password").value;
 
     localStorage.setItem("storedEmail", email);
     localStorage.setItem("storedPassword", password);
-  }
+
+    // ログイン成功時のその他の処理...
+    window.alert("あかうんと。はつくれないよ");
+  };
+
+  // パスワードの可視化スイッチ
+  window.pushHideButton = function () {
+    var txtPass = document.getElementById("js-password");
+    var btnEye = document.getElementById("buttonEye");
+    if (txtPass.type === "password") {
+      txtPass.type = "text";
+      btnEye.className = "fa fa-eye-slash";
+    } else {
+      txtPass.type = "password";
+      btnEye.className = "fa fa-eye";
+    }
+  };
 })();
