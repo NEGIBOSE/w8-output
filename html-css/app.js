75f282632473971bfc5141708200035a00f6fef3
NEGIBOSE <ryo0827ngsh@icloud.com>
2023-12-09 00:56:43 +0900

    typeをbuttonからinputに変更

--- html-css/app.js
+++ html-css/app.js
@@ -1,12 +1,33 @@
 (() => {
-  // ログイン関数
+  window.addEventListener("load", () => {
+    // ローカルストレージから入力されたデータを取得
+    const email = localStorage.getItem("email");
+    const password = localStorage.getItem("password");
+
+    // emailとパスワードが一致しているか
+    if (email && password) {
+      // 一致していたら表示する
+      const $email = document.getElementById("js-email");
+      const $password = document.getElementById("js-password");
+      $email.value = email;
+      $password.value = password;
+    }
+  });
+
+  // Submit event listener
+  const $form = document.getElementById("js-form");
+  $form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    // 入力されたデータを取得
+    const formData = new FormData($form);
+
+    // 入力されたデータをローカルストレージに保存
+    localStorage.setItem("email", formData.get("email"));
+    localStorage.setItem("password", formData.get("password"));
+  });
+
   window.signIn = function () {
-    const email = document.getElementById("js-email").value;
-    const password = document.getElementById("js-password").value;
-
-    localStorage.setItem("storedEmail", email);
-    localStorage.setItem("storedPassword", password);
-
     // ログイン成功時のその他の処理...
     window.alert("あかうんと。はつくれないよ");
   };
